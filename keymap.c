74dc4611b5cabd83ce94c08e58dabd9356e8186b
Bernard (Home) <bernard@emptystring.net>
2018-09-29 01:07:52 +0800

    20180929 first draft

--- keymap.c
+++ keymap.c
@@ -7,12 +7,14 @@ extern keymap_config_t keymap_config;
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _QWERTY 0
-#define _NAV 2
+#define _LOWER 2
+#define _TOOLS 3
 
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   NAV,
+  TOOLS
 
 };
 
@@ -28,40 +30,62 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Tab  |   Q  |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  |   \  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |CAPS(NAV)|A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |  "   |
+   * |CtEnt |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   '  |(none)|
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |   /  |Enter |
+   * | Sh(  |   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |   /  | Sh)  |
    * |------+------+------+------+------+------+------..-----+------+------+------+------+------+------|
-   * | Ctrl | GUI  | Alt  |RGBTOG| NAV  |Space |Delete||Enter|Space |  NAV |   -  |   =  | PGUP | PGDN |
+   * | L2   | GUI  | Alt  |(none)| LOWER| Bspc |CtDel ||Enter|Space |(none)|  Left| Up   | Down | Right|
    * `------------------------------------------------''-----------------------------------------------'
    */
   [_QWERTY] = LAYOUT( \
-    KC_GESC,          KC_1,    KC_2,    KC_3,    KC_4,     KC_5,                       KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    KC_BSPC, \
-    KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,                       KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_BSLS, \
-    LT(_NAV, KC_CAPS),KC_A,    KC_S,    KC_D,    KC_F,     KC_G,                       KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-    KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,                       KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
-    KC_LCTL,          KC_LGUI, KC_LALT, RGB_TOG, MO(_NAV), KC_SPC,  KC_DEL,  KC_ENT,   KC_SPACE, MO(_NAV), KC_MINS, KC_EQL,  KC_PGUP, KC_PGDN \
+    KC_GESC,          KC_1,    KC_2,    KC_3,     KC_4,       KC_5,                                 KC_6,     KC_7,        KC_8,    KC_9,    KC_0,    KC_MINS, \
+    KC_TAB,           KC_Q,    KC_W,    KC_E,     KC_R,       KC_T,                                 KC_Y,     KC_U,        KC_I,    KC_O,    KC_P,    KC_BSLS, \
+    LCTL(KC_ENT),     KC_A,    KC_S,    KC_D,     KC_F,       KC_G,                                 KC_H,     KC_J,        KC_K,    KC_L,    KC_QUOT, _______, \
+    KC_LSPO,          KC_Z,    KC_X,    KC_C,     KC_V,       KC_B,                                 KC_N,     KC_M,        KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, \
+    LALT(KC_LSFT),    KC_LGUI, KC_LALT, _______,  MO(_LAYER), KC_BSPC, LCTL_T(KC_SPACE),  KC_ENT,   KC_SPACE, MO(_TOOLS),  KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT \
   ),
 
-  /* NAV
+  /* LOWER
    * ,-----------------------------------------.             .-----------------------------------------.
-   * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |             |  F7  |  F8  |  F9  |  F10 |  F11 | F12  |
+   * |      |  F1  |  F2  |  F3  |  F4  |  F5  |             |      |      |      |      |      |      |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |RGBSAI|RGBVAI|RGBSAD| RESET|  [   |             |   ]  | Pgup |  Up  | Pgdn |Insert| Home |
+   * |      |   !  |   @  |   #  |   $  |   %  |             |   Del| Home |  Up  | End  |      |      |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |RGBHUD|RGBVAD|RGBHUI|      |      |             |      | Left | Down | Right|Delete| End  |
+   * |      |  ^   |  &   |  *   |  =   |  [   |             |   ]  |Left  | Down |  Rt  |   ;  |      |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |Shift |      |      |      |      |      |             | NKRO |      |      | Pause| Back | Next |
+   * |      |      |      |      |      |      |             |      | CtIns|      |      |      |      |
    * |------+------+------+------+------+------+------..-----+------+------+------+------+------+------|
-   * | Ctrl | GUI  | Alt  |RGBMOD|      |      |      ||     |      |      |      | Mute | VOLUP| VOLDN|
+   * | WinL |      |      |      |      |      |      ||     |      |      |      |      |      |CtAltD|
    * `------------------------------------------------''-----------------------------------------------'
    */
-  [_NAV] = LAYOUT( \
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,             KC_F8,    KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
-    _______, RGB_SAI, RGB_VAI, RGB_SAD, RESET,   KC_LBRC,                   KC_RBRC,           KC_PGUP,  KC_UP,   KC_PGDN, KC_INS,  KC_HOME, \
-    _______, RGB_HUD, RGB_VAD, RGB_HUI, _______, _______,                   _______,           KC_LEFT,  KC_DOWN, KC_RGHT, KC_DEL,  KC_END,  \
-    KC_LSFT, _______, _______, _______, _______, _______,                   MAGIC_TOGGLE_NKRO, _______,  _______, KC_MPLY, KC_MPRV, KC_MNXT, \
-    KC_LCTL, KC_LGUI, KC_LALT, RGB_MOD, _______, _______, _______, _______, _______,           _______,  _______, KC_MUTE, KC_VOLU, KC_VOLD \
+  [_LOWER] = LAYOUT( \
+    _______,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     _______, _______,       _______, _______, _______, _______, \
+    _______,    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_DEL,  KC_HOME,       KC_UP,   KC_END,  _______, _______, \
+    _______,    KC_CIRC, KC_AMPR, KC_ASTR, KC_EQL,  KC_LBRC,                   KC_RBRC, KC_LEFT,       KC_DOWN, KC_RGHT, KC_SCLN, _______, \
+    _______,    _______, _______, _______, _______, _______,                   _______, LCTL(KC_INS),  _______, _______, _______, _______, \
+    LGUI(KC_L), _______, _______, _______, _______, _______, _______, _______, _______, _______,       _______, _______, _______, LCTL(LALT(KC_DEL)) \
+  ),
+
+  /* TOOLS
+   * ,-----------------------------------------.             .-----------------------------------------.
+   * |      |      |      |      |      |      |             |      |      |      |      |      |      |
+   * |------+------+------+------+------+------|             |------+------+------+------+------+------|
+   * |      |RGBSAI|RGBVAI|RGBSAD| RESET|      |             |      |      |      |      |      |      |
+   * |------+------+------+------+------+------|             |------+------+------+------+------+------|
+   * |      |RGBHUD|RGBVAD|RGBHUI|RGBTOG|      |             |      |      |      |      |      |      |
+   * |------+------+------+------+------+------|             |------+------+------+------+------+------|
+   * |      |      |      |      |      |      |             |      |      |      | Pause| Back | Next |
+   * |------+------+------+------+------+------+------..-----+------+------+------+------+------+------|
+   * |      |      |      |RGBMOD|      |      |      ||     |      |      |      | Mute | VOLUP| VOLDN|
+   * `------------------------------------------------''-----------------------------------------------'
+   */
+  [_TOOLS] = LAYOUT( \
+    _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
+    _______, RGB_SAI, RGB_VAI, RGB_SAD, RESET,   _______,                   _______, _______, _______, _______, _______, _______, \
+    _______, RGB_HUD, RGB_VAD, RGB_HUI, RGB_TOG, _______,                   _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______,                   _______, _______, _______, KC_MPLY, KC_MPRV, KC_MNXT, \
+    _______, _______, _______, RGB_MOD, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLU, KC_VOLD \
+  ),
   ),
 
 };
